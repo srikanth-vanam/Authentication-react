73cedf62829f24fc0290e6ea82eb17f6ec0e342a
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-24 22:34:33 +0530

    handled login, signUp with firebase signIn,signUp API's respectively

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,45 +1,97 @@
-import { useState, useRef } from 'react';
+import { useState, useRef } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoad,setIsLoad]=useState(false);
+  const [isLoad, setIsLoad] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-  const submitHandler=(e)=>{
+
+  const submitHandler = (e) => {
     e.preventDefault();
+    // setIsLoad(true);
+    const obj = {
+      email: emailInputRef.current.value,
+      password: passwordInputRef.current.value,
+    };
+    postDataHandler(obj);
+  };
+
+  const postDataHandler = (obj) => {
     setIsLoad(true);
-  }
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC6fdqT-BKSYvdgNjdso0biEIf45XQLPXk";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC6fdqT-BKSYvdgNjdso0biEIf45XQLPXk";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: obj.email,
+        password: obj.password,
+        returnSecureToken: true,
+      }),
+    })
+      .then((res) => {
+        setIsLoad(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMsg = "Authentication failed";
+            if(data && data.error && data.error.message){
+              errorMsg=data.error.message;
+            }
+            throw new Error(errorMsg);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor="password">Your Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
+            ref={passwordInputRef}
             required
           />
         </div>
         <div className={classes.actions}>
-        {isLoad && <p>Sending request....</p>}
-        {!isLoad && <button
-        type='submit'>
-          Submit
-          </button>}
-           <button
-            type='button'
+          {isLoad && <p>Sending request....</p>}
+          {!isLoad && (
+            <button type="submit">
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )}
+          <button
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
